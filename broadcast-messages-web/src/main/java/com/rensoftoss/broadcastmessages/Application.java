c697265aac1de9550a62452de497a13d96a10763
Gyorgy Busak <busak@freemail.hu>
2016-06-14 03:25:04 +0200

    add cors configuration for remote test

--- broadcast-messages-web/src/main/java/com/rensoftoss/broadcastmessages/Application.java
+++ broadcast-messages-web/src/main/java/com/rensoftoss/broadcastmessages/Application.java
@@ -2,17 +2,27 @@ package com.rensoftoss.broadcastmessages;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.cors.CorsConfiguration;
 
 @SpringBootApplication
 @ImportResource(value = {
         "classpath:spring-configs/common-config.xml",
         "classpath:spring-configs/message-websocket-config.xml",
         "classpath:spring-configs/rest-to-kafka-config.xml",
-        "classpath:spring-configs/kafka-to-db-config.xml"
+        "classpath:spring-configs/kafka-to-db-config.xml",
+        "classpath:spring-configs/kafka-to-websocket-config.xml"
 })
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CorsConfiguration cors() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        return corsConfiguration;
     }
 }
